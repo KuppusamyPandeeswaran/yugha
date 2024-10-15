8238414f7cd574a2c7db7510d93171a8bc255663
KuppusamyPandeeswaran <pandeeswarankuppusamy@gmail.com>
2024-10-15 17:51:06 +0530

    dummy test

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,55 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <pre>
+      ugha coaching center
+
+யுகா கோச்சிங் சென்டர் 
+
+
+சிறப்பு அம்சங்கள் :
+
+தேர்வுகளை அதிக அளவில் எதிர்கொண்ட அனுபவமுள்ள திறமைமிக்க ஆசிரியர்கள்  
+
+தமிழ் தகுதி தேர்வுக்கு சிறப்பு  கவனம் அளிக்கப்படுகிறது
+
+முறையான தினசரி  தேர்வுகள் மற்றும் அலகு  தேர்வுகள்
+
+பயிற்சி கையேடுகள் வழங்கப்படும் / நூலக வசதி உண்டு 
+
+பிற பயிற்சி மையங்களை காட்டிலும் மிக குறைவான கட்டணம் 
+
+
+ 
+ 
+தொடர்புக்கு: 99946 45926, 99420 09644
+
+தென்னம்பட்டி ரோடு, பைபாஸ் அருகில் வடமதுரை 
+
+புதிய வகுப்புகுளுக்கு அட்மிஷன் நடைபெறுகிறது 
+
+
+
+
+Special features:
+
+Experienced and talented teachers who have faced a lot of exams
+
+Special attention is given to the Tamil Eligibility Test
+
+Formal daily tests and unit tests
+
+Training manuals are provided / library facility is available
+
+Fees are very less than other coaching centers
+
+Contact: 99946 45926, 99420 09644
+
+Thannampatti Road, Near Bypass, Vadamadurai
+
+
+Admission is taking place for new classes
+      </pre>
     </>
   )
 }
